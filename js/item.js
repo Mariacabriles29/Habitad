294c78cbc66f6cd325339115023f854eb55326cb
Mariacabriles29 <mecp2992@gmail.com>
2023-03-03 16:23:55 -0430

    fix: :hammer: fix in problems with nav bar

--- js/item.js
+++ js/item.js
@@ -1,16 +1,10 @@
 
-document.getElementById("services").addEventListener("click", function(event) {
-    event.preventDefault();
-    var imageServices = document.querySelector(".image-services");
-    imageServices.style.display = "block";
-  });
-  
-  document.addEventListener("click", function(event) {
-    var elemento = event.target;
-    var imageServices = document.querySelector(".image-services");
-    if (!elemento.closest(".navbar") && imageServices.style.display === "block") {
-        imageServices.style.display = "none";
-    }
-  });
-  
+function hideImages() {
+  const images = document.querySelectorAll(".dropdown-item img");
+  images.forEach((image) => (image.style.display = "none"));
+  document.querySelector('.dropdown-menu').classList.toggle('show');
+
+
+}
+
   
